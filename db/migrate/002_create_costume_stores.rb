bb646ebdcfb2dca0255e6d0107be8b2b9ca5dfe4
chic-caller-7520 <arbrown110@gmail.com>
2020-06-03 23:59:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :in_business
+      t.datetime :opening
+      t.datetime :closing
+      t.timestamps null :false
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.float :price
+      t.string :theme
+      t.boolean :kid_friendly
+      t.datetime :opening
+      t.datetime :closing
+      t.text :description
+      t.timestamps null :false
+    end
+  end
+end
